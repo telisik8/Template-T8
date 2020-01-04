54f80dc665f5e40beb528228dd77a6e0ab915710
Telisik8 <59242165+telisik8@users.noreply.github.com>
2020-01-04 17:10:03 +0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,38 @@
-
+{
+  "name": "Telisik-8",
+  "short_name": "Telisik-8",
+  "start_url": "https://telisik8.com/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#C51162",
+  "description": "Informasi Faktual dan Berimbang.",
+  "icons": [{
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/48/logoT8.png",
+    "sizes": "48x48",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/s72/logoT8.png",
+    "sizes": "72x72",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/s69/logoT8.png",
+    "sizes": "96x96",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/s144/logoT8.png",
+    "sizes": "144x144",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/s168/logoT8.png",
+    "sizes": "168x168",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/s192/logoT8.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-ZYoc8L_fxik/XhBW2YpsJQI/AAAAAAAABbk/TLtMdC-kUkgy6SZUbAfJVgdc7wY8EyBJwCEwYBhgL/s512/logoT8.png",
+    "sizes": "512x512",
+    "type": "image/png"
+  }]
+}
